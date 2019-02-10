805e0686ea1ec2c3d787f47d3aeaa25b6be8982a
Lev Kowalski <levkowalski@gmail.com>
2019-02-10 17:18:22 +0100

    wip

--- introduce.hs
+++ introduce.hs
@@ -13,5 +13,21 @@ howAreYou = putStrLn "What is your name ?"
                 >>= \_ -> getLine
                 >>= \name -> putStrLn $ "hello " ++ name ++ "!"
 
-main = howAreYou
+howAreYou' = putStrLn "What is your name ?"
+                >> getLine
+                >>= \name -> putStrLn $ "hello " ++ name ++ "!"
 
+howAreYou'' = do
+                putStrLn "What is your name ?"
+                name <- getLine
+                putStrLn $ "hello " ++ name ++ "!"
+
+
+
+howAreYou''' = do
+                putStrLn "What is your name ?"
+                name <- getLine
+                let phrase = "hello" ++ name ++ " !"
+                putStrLn phrase
+
+main = howAreYou'''
