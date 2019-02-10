2dac7e38444da8996c1c315dacfd7381492ed6ea
Lev Kowalski <levkowalski@gmail.com>
2019-02-10 17:09:40 +0100

    first

---
+++ introduce.hs
@@ -0,0 +1,17 @@
+main :: IO ()
+
+hellWorld :: IO ()
+hellWorld = putStrLn "Hello world"
+
+introduce :: String -> String -> IO ()
+introduce name1 name2 = do
+  putStrLn (name1 ++ ", this is " ++ name2)
+  putStrLn (name2 ++ ", this is " ++ name1)
+
+
+howAreYou = putStrLn "What is your name ?"
+                >>= \_ -> getLine
+                >>= \name -> putStrLn $ "hello " ++ name ++ "!"
+
+main = howAreYou
+
