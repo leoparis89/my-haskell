696696ae8312d25bfd82aec0fde08f34f0cfb8af
Lev Kowalski <levkowalski@gmail.com>
2019-02-10 20:38:13 +0100

    recursion

--- recursion.hs
+++ recursion.hs
@@ -4,4 +4,13 @@ repeatString str n =
         else str ++ (repeatString str (n-1))
 
 
+
+
+pow2 n = pow2loop n 1 0
+pow2loop n x i =
+    if i<n
+        then pow2loop n (x*2) (i+1)
+        else x
+
+
 main = putStrLn $ repeatString "foo" 4
